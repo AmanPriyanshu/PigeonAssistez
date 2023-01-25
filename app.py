a394ff54c10051e36c1252cef9acdd2aaa8f237c
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2023-01-25 03:22:38 +0300

    base app - reset

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import streamlit as st
 from streamlit_chat import message
+import datetime
 
 def get_text():
 	input_text = st.text_input("You: ","Hello, how are you?", key="input")
 	return input_text 
 
 def query(params):
+	if params["desc"] == "None":
+		pass #analyze using NLP thingy
+	else:
+		return "We got da time"+str(params["date"])+" "+str(params["time"])+"-->"+str(params["desc"])
 	return "Lmao"
 
 def main():
+	st.title("Chat with our Bespoke Pigeon")
+	st.markdown("##### We use Courier to deliver your messages, whereas our in-house chatbot helps organize your schedules amonst individuals or your slack team.")
 	with st.sidebar:
 		st.image('logo.png')
 		courier_auth_token = st.text_input("Enter Courier Auth-Token", type="password")
@@ -19,7 +26,17 @@ def main():
 	if 'past' not in st.session_state:
 		st.session_state['past'] = []
 
-	col1, col2 = st.columns([5, 1])
+	with st.expander("Schedule your appointment (manually)"):
+		col_a, col_b, col_c = st.columns([1, 1, 4])
+		with col_a:
+			d = st.date_input("Enter Date", datetime.datetime.now().date())
+		with col_b:
+			t = st.time_input('Enter Time', datetime.datetime.now().time())
+		with col_c:
+			placeholder = st.empty()
+			desc = placeholder.text_input("Enter Details", "None", key="description")
+
+	col1, col2, col3 = st.columns([7, 1, 1])
 	with col1:
 		user_input = get_text()
 	with col2:
@@ -27,12 +44,18 @@ def main():
 		st.text("")
 		if st.button("Send"):
 			output = query({
-					"past_user_inputs": st.session_state.past,
-					"generated_responses": st.session_state.generated,
+					"date": d,
+					"time": t,
+					"desc": desc,
 					"text": user_input,
 				})
 			st.session_state.past.append(user_input)
 			st.session_state.generated.append(output)
+	with col3:
+		st.text("")
+		st.text("")
+		if st.button("Reset"):
+			desc = placeholder.text_input('text', value='None', key="description")
 
 	if st.session_state['generated']:
 		for i in range(len(st.session_state['generated'])-1, -1, -1):
